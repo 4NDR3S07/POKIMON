d58115eed85fa503339b941a314c165583724614
Your Name <you@example.com>
2025-03-25 15:05:17 -0500

    Andres

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function Loguear() {
+    let username = document.getElementById('username').value
+    let password = document.getElementById('clave').value
+
+    if(username == 'Jhan' && password == 'taco'){
+        alert('Usuario valido')
+    }else{
+        alert('usuario incorrecto')
+    }  
+
+}
+
+
